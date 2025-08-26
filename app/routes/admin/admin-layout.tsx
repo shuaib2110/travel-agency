0c1e6045c98ee784bb2f0a943506a0cd88b7dcf3
Shuaib Ahmad <shuaibdevloper@gmail.com>
2025-08-26 21:35:15 +0530

    Routing

---
+++ app/routes/admin/admin-layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+const AdminLayout = () => {
+  return (
+    <div className='admin-layout'>
+      MobileSidebar
+      <aside className='w-full max-w-[270px] hidden lg:block'> sidebar</aside>
+      <aside className='children'>
+        <Outlet />
+      </aside>
+    </div>
+  )
+}
+
+export default AdminLayout
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,8 @@
+
+const dashboard = () => {
+  return (
+    <div className="w-full h-screen bg-amber-950 text-amber-50">dashboard page</div>
+  )
+}
+
+export default dashboard
