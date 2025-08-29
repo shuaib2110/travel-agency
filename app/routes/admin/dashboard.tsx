88616e127e5db2be8d8d5b282458b9c7afcd8b41
Shuaib Ahmad <shuaibdevloper@gmail.com>
2025-08-29 21:34:47 +0530

    Admin dashboard in progress

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,16 +1,44 @@
-import Header from "components/Header"
+import { StatsCard, TripCard } from "components";
+import Header from "components/Header";
 
-// It's a convention to name React components with PascalCase (Dashboard)
+
+
 const Dashboard = () => {
+
+  const { totalUsers, userJoined, totalTrips, tripsCreated, userRole } =
+    dashboardStats;
   return (
-    <div className="dashboard wrapper">
+    <main className="dashboard wrapper">
       {/* Pass the required props here */}
       <Header
-        title="Welcome to the Dashboard"
+        title={`Welcome  ${user.name}  to the Dashboard`}
         description="Here's a summary of your recent activity."
       />
-    </div>
-  )
-}
+      <section className="flexx flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="Total Users"
+            total={totalUsers}
+            currentMonthCount={userJoined.currentMonth}
+            lastMonthCount={userJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Total Trips"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Active Users today"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+      </section>
+      <TripCard />
+    </main>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
